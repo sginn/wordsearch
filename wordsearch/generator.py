7815cd67b9efd532ddc6d569b56f55c163f030d1
Steven Ginn <steven.ginn@locationlabs.com>
2016-11-14 17:59:59 -0800

    Small refactor to mark some methods “private” and move to bottom of module

--- wordsearch/generator.py
+++ wordsearch/generator.py
@@ -5,11 +5,6 @@ from random import choice, randrange, sample, shuffle
 from validate import validate
 
 
-def init_grid(size):
-    ''' create a square grid '''
-    return [[EMPTY_SPACE for x in range(size)] for y in range(size)]
-
-
 def generate_wordsearch(wordlist, difficulty=1):
     ''' Creates a wordsearch and returns the generated grid and an answer key
 
@@ -20,14 +15,14 @@ def generate_wordsearch(wordlist, difficulty=1):
     shuffle(wordlist)
 
     # Start the grid with size equal to longest word in list
-    grid = init_grid(difficulty * max(map(len, wordlist)))
+    grid = _init_grid(difficulty * max(map(len, wordlist)))
 
     while not place_words(grid, words_left=wordlist):
         # we couldn't find a solution, so make the grid bigger and try again
         print "Couldn't find a solution with size", len(grid), "Going bigger and trying again"
-        grid = init_grid(len(grid)+1)
+        grid = _init_grid(len(grid)+1)
 
-    answer_key = copy_grid(grid)
+    answer_key = _copy_grid(grid)
 
     fill_in(grid, wordlist)
 
@@ -35,75 +30,13 @@ def generate_wordsearch(wordlist, difficulty=1):
     print "Validating grid"
     while not validate(grid, wordlist) and failure_count < 5:
         failure_count += 1
-        wordsearch = copy_grid(answer_key)
+        wordsearch = _copy_grid(answer_key)
         fill_in(wordsearch, wordlist, failure_count)
 
     if failure_count == 5:
         return generate_wordsearch(wordlist, difficulty)
 
     return grid, answer_key
-
-
-def word_can_fit(grid, word, x, y, direction):
-
-    ''' Checks to see if the given word can fit in the direction given from a start position
-
-    grid: The grid to check, really only need grid width/height
-    word: the word to check
-    x, y: Coordinates to start from
-    direction: one of the 8 directions are we assessing
-
-    '''
-    # would word placement go beyond edge of grid?
-    end_x = len(word) * direction[0] + x
-    end_y = len(word) * direction[1] + y
-
-    if end_x < 0 or end_x > len(grid):
-        return False
-
-    if end_y < 0 or end_y > len(grid):
-        return False
-
-    # check placement of letters, if we hit a populated cell with a mismatched letter, fail
-    for letter in word:
-        if grid[x][y] == letter or grid[x][y] == EMPTY_SPACE:
-            x += direction[0]
-            y += direction[1]
-        else:
-            return False
-
-    return True
-
-
-def copy_grid(grid):
-    ''' Generate a clone of the grid '''
-    new_grid = init_grid(len(grid))
-    return transfer_cells(new_grid, grid)
-
-
-def transfer_cells(grid, backtrack_grid):
-    ''' Restore grid cell data with info from the saved backtrack_grid '''
-    for x in range(len(backtrack_grid)):
-        for y in range(len(backtrack_grid)):
-            grid[x][y] = backtrack_grid[x][y]
-    return grid
-
-
-def insert_word(grid, word, xpos, ypos, direction):
-    '''Place the word on the grid
-
-    grid:
-    word: the word to place
-    xpos, ypos: starting position
-    direction: The direction to place letters
-
-    Note: Assumes the word fits without collisions!
-    '''
-    for letter in word:
-        grid[xpos][ypos] = letter
-        xpos += direction[0]
-        ypos += direction[1]
-    return grid
 
 
 def fill_in(grid, wordlist, sample_size=8):
@@ -152,19 +85,86 @@ def place_words(grid, words_left):
 
         # try to place the word
         for direction in directions:
-            if word_can_fit(grid, word_to_place, xpos, ypos, direction=direction):
+            if _word_can_fit(grid, word_to_place, xpos, ypos, direction=direction):
 
                 # hold onto a copy of this grid in case we need to revert
-                backtrack_grid = copy_grid(grid)
+                backtrack_grid = _copy_grid(grid)
 
                 # place the word, and recurse
-                insert_word(grid, word_to_place, xpos, ypos, direction)
+                _insert_word(grid, word_to_place, xpos, ypos, direction)
                 if place_words(grid, words_left=words_left[1:]):
                     # we placed all remaining words so return success!
                     return True
                 else:
                     # remaining words couldn't fit, so undo the insertion and try next direction
-                    transfer_cells(grid, backtrack_grid)
+                    _transfer_cells(grid, backtrack_grid)
 
     # we exhausted all positions and directions for this word, backtrack...
     return False
+
+
+def _init_grid(size):
+    ''' create a square grid '''
+    return [[EMPTY_SPACE for x in range(size)] for y in range(size)]
+
+
+def _word_can_fit(grid, word, x, y, direction):
+
+    ''' Checks to see if the given word can fit in the direction given from a start position
+
+    grid: The grid to check, really only need grid width/height
+    word: the word to check
+    x, y: Coordinates to start from
+    direction: one of the 8 directions are we assessing
+
+    '''
+    # would word placement go beyond edge of grid?
+    end_x = len(word) * direction[0] + x
+    end_y = len(word) * direction[1] + y
+
+    if end_x < 0 or end_x > len(grid):
+        return False
+
+    if end_y < 0 or end_y > len(grid):
+        return False
+
+    # check placement of letters, if we hit a populated cell with a mismatched letter, fail
+    for letter in word:
+        if grid[x][y] == letter or grid[x][y] == EMPTY_SPACE:
+            x += direction[0]
+            y += direction[1]
+        else:
+            return False
+
+    return True
+
+
+def _copy_grid(grid):
+    ''' Generate a clone of the grid '''
+    new_grid = _init_grid(len(grid))
+    return _transfer_cells(new_grid, grid)
+
+
+def _transfer_cells(grid, backtrack_grid):
+    ''' Restore grid cell data with info from the saved backtrack_grid '''
+    for x in range(len(backtrack_grid)):
+        for y in range(len(backtrack_grid)):
+            grid[x][y] = backtrack_grid[x][y]
+    return grid
+
+
+def _insert_word(grid, word, xpos, ypos, direction):
+    '''Place the word on the grid
+
+    grid:
+    word: the word to place
+    xpos, ypos: starting position
+    direction: The direction to place letters
+
+    Note: Assumes the word fits without collisions!
+    '''
+    for letter in word:
+        grid[xpos][ypos] = letter
+        xpos += direction[0]
+        ypos += direction[1]
+    return grid
